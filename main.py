ddccd606706089ba3e2d84df4d1731d62ebdcc22
Carlos Hernandez <c.hernandez@raventoscodorniu.com>
2024-03-01 12:49:36 +0100

    Refactor code to improve performance and readability

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,16 +6,16 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+  return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+  return {"item_id": item_id, "q": q}
 
-#leer todos los registros
+# Read all records
 
 @app.get("/products")
 def get_all_products():
-    cursor = "product-1"
-    return cursor
+  cursor = "product-1"
+  return cursor
